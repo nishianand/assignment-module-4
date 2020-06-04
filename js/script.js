2e25ddf5aac49502d410f1be3a73989ea8c88c47
nishianand <47775251+nishianand@users.noreply.github.com>
2020-06-05 00:31:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log('Hello Yaakov');
+console.log('Good Bye John');
+console.log('Good Bye Jen');
+console.log('Good Bye Jason');
+console.log('Hello Paul');
+console.log('Hello Frank');
+console.log('Hello Larry');
+console.log('Hello Paula');
+console.log('Hello Laura');
+console.log('Good Bye Jim');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
